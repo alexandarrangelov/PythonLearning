cdb6f0ce93ac6df9a196d397b52096c47ddde4cb
alexandarrangelov <57296266+alexandarrangelov@users.noreply.github.com>
2019-11-02 16:55:57 +0000

    Add files via upload
    Simple "Rock Paper Scissors"

---
+++ RPS Project.py
@@ -0,0 +1,36 @@
+
+print('...rock...')
+print('...paper...')
+print('...scissors...')
+
+player1 = input('(Player 1 choice : )')
+player2 = input('(Player 2 choice : )')
+
+
+if player1=="rock" and player2=="scissors":
+	print('Player1 wins!')
+	pass
+elif player1=="rock" and player2=="paper":
+	print('Player2 wins!')
+elif player1 == "rock" and player2=="scissors":
+	print('Player1 wins!')
+elif player1=="paper" and player2=="scissors":
+	print('Player2 wins!')
+elif player1 == player2:
+	print('It is a tie!')
+else :
+	print('Something went wrong!')
+
+
+#       1.rock-scissors ->rock
+#		2.rock-paper ->paper
+#		3.rock-scissors ->rock
+#		4.paper-scissors
+#
+#
+#
+#
+#
+#
+#
+#
